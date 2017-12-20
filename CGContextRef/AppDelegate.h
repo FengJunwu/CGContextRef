7d07e7879fbd8fadbf93e4469fae76e6e820c844
Feng Junwu <13659214905@163.com>
2017-12-20 16:38:57 +0800

    Initial Commit

---
+++ CGContextRef/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CGContextRef
+//
+//  Created by JND on 2017/12/20.
+//  Copyright © 2017年 JND. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
